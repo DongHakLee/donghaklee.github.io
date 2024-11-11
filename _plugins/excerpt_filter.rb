2966f9bd1bfa2c5507df6532886460a90b81abe8
DongHakLee <taejonldh@nate.com>
2024-11-11 11:55:27 +0900

    Deploy all pending articles

--- _plugins/excerpt_filter.rb
+++ _plugins/excerpt_filter.rb
@@ -1,9 +1,13 @@
-module Jekyll
-  class ExcerptFilter
-    def self.clean_excerpt(page)
-      # 페이지의 내용을 문자열로 가져오기
-      content = page.output.to_s.dup
+# _plugins/excerpt_filter.rb
+require 'cgi'
 
+module Jekyll
+  module ExcerptFilter
+    def clean_excerpt(input)
+      # 입력값 검증
+      return "" if input.nil?
+      content = input.to_s.dup
+      
       # Markdown 이미지 제거
       content.gsub!(/!\[(?:.*?)\]\((?:.*?)\)/, '')
       
@@ -18,22 +22,18 @@ module Jekyll
       content.gsub!(/\[([^\]]*?)\]\((?:.*?)\)/, '\1') # 링크
       content.gsub!(/^#+\s*(.*)$/m, '\1')           # 헤더
       
-      # HTML 태그 제거 (개선된 방식)
+      # HTML 태그 제거
       content.gsub!(/<(script|style)[^>]*>.*?<\/\1>/mi, '')
       content.gsub!(/<[^>]*>/, '')
-
+      
       # 특수문자 및 공백 처리
       content = CGI.unescapeHTML(content)
       content.gsub!(/\s+/, ' ')
       content.strip!
-
-      # 페이지의 output을 업데이트
-      page.output = content
+      
+      content
     end
   end
 end
 
-# 후크 등록
-Jekyll::Hooks.register [:pages, :posts, :documents], :post_render do |page|
-  ExcerptFilter.clean_excerpt(page)
-end
+Liquid::Template.register_filter(Jekyll::ExcerptFilter)
